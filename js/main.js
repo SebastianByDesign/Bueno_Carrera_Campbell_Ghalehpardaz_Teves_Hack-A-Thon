2fed5275d5809057adfc9f8106242f73422e4ca4
unknown <guilhermebueno6@gmail.com>
2019-10-07 16:30:16 -0400

    incorporated the JS changes for the 3D image and the custom cursor

--- js/main.js
+++ js/main.js
@@ -1,9 +1,35 @@
-//Sidebar functionality
-function toggle() {
-    document.getElementById("nav-toggler").classList.toggle("fa-times");
-    document.getElementById("nav-toggler").classList.toggle("fa-bars");
-    document.getElementById("sidebar").classList.toggle("active");
-}
+//Sidebar functionality(()=>{
+  (()=>{  
+    console.log('fired!');
+//     let numbers = document.querySelector("img");
 
-var openMenu = document.getElementById('nav-toggler');
-openMenu.addEventListener("click", toggle);
+// function loggin(){
+//     var x = event.clientX 
+//     // console.log(x)
+//     var newX = x/16
+//     var roundedX = Math.floor(newX)
+//     console.log(roundedX)
+//     if (roundedX>90){
+//         return
+//     }
+
+
+
+//     numbers.src = `images/FramesCube/CubeFrame${roundedX}.png`;
+    
+// }
+//     addEventListener("mousemove", loggin);
+
+    function toggle() {
+        document.getElementById("nav-toggler").classList.toggle("fa-times");
+        document.getElementById("nav-toggler").classList.toggle("fa-bars");
+        document.getElementById("sidebar").classList.toggle("active");
+    }
+    
+    var openMenu = document.getElementById('nav-toggler');
+    openMenu.addEventListener("click", toggle);
+
+})();
+
+
+
