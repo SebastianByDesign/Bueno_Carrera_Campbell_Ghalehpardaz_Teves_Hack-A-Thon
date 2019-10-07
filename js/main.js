ae83d0937c519e8f7b73c76ea91a6e809cc6e00a
Walter Sebastian Carrera <cwalter1994@gmail.com>
2019-10-07 15:03:55 -0400

    Sidebar and Hero Complete
    Mobile sidebar navigation added
    Placeholder hero section added

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//Sidebar functionality
+function toggle() {
+    document.getElementById("nav-toggler").classList.toggle("fa-times");
+    document.getElementById("nav-toggler").classList.toggle("fa-bars");
+    document.getElementById("sidebar").classList.toggle("active");
+}
+
+var openMenu = document.getElementById('nav-toggler');
+openMenu.addEventListener("click", toggle);
