9a99cca6dda2843d270c770d4de377deaed22c55
unknown <guilhermebueno6@gmail.com>
2019-10-08 02:25:08 -0400

    fixing the NPM start

--- js/main.js
+++ js/main.js
@@ -20,14 +20,14 @@ function loggin(){
 }
     addEventListener("mousemove", loggin);
 
-    // function toggle() {
-    //     document.getElementById("nav-toggler").classList.toggle("fa-times");
-    //     document.getElementById("nav-toggler").classList.toggle("fa-bars");
-    //     document.getElementById("sidebar").classList.toggle("active");
-    // }
+    function toggle() {
+        document.getElementById("nav-toggler").classList.toggle("fa-times");
+        document.getElementById("nav-toggler").classList.toggle("fa-bars");
+        document.getElementById("sidebar").classList.toggle("active");
+    }
     
-    // var openMenu = document.getElementById('nav-toggler');
-    // openMenu.addEventListener("click", toggle);
+    var openMenu = document.getElementById('nav-toggler');
+    openMenu.addEventListener("click", toggle);
 
 })();
 
