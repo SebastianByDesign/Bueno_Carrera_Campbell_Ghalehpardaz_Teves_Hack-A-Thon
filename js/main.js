5318b245709ad8e61152dda47c64d0a7b7ca955a
unknown <guilhermebueno6@gmail.com>
2019-10-08 01:42:23 -0400

    made the 3D image of the product Interactive through JS scripting

--- js/main.js
+++ js/main.js
@@ -1,33 +1,33 @@
 //Sidebar functionality(()=>{
   (()=>{  
     console.log('fired!');
-//     let numbers = document.querySelector("img");
+    let numbers = document.querySelector(".InteractiveImage img");
 
-// function loggin(){
-//     var x = event.clientX 
-//     // console.log(x)
-//     var newX = x/16
-//     var roundedX = Math.floor(newX)
-//     console.log(roundedX)
-//     if (roundedX>90){
-//         return
-//     }
-
-
-
-//     numbers.src = `images/FramesCube/CubeFrame${roundedX}.png`;
-    
-// }
-//     addEventListener("mousemove", loggin);
-
-    function toggle() {
-        document.getElementById("nav-toggler").classList.toggle("fa-times");
-        document.getElementById("nav-toggler").classList.toggle("fa-bars");
-        document.getElementById("sidebar").classList.toggle("active");
+function loggin(){
+    var x = event.clientX 
+    // console.log(x)
+    var newX = x/16
+    var roundedX = Math.floor(newX)
+    console.log(roundedX)
+    if (roundedX>90){
+        return
     }
+
+
+
+    numbers.src = `http://127.0.0.1:5500/images/FramesCube/MainComp_000${roundedX}.png`;
     
-    var openMenu = document.getElementById('nav-toggler');
-    openMenu.addEventListener("click", toggle);
+}
+    addEventListener("mousemove", loggin);
+
+    // function toggle() {
+    //     document.getElementById("nav-toggler").classList.toggle("fa-times");
+    //     document.getElementById("nav-toggler").classList.toggle("fa-bars");
+    //     document.getElementById("sidebar").classList.toggle("active");
+    // }
+    
+    // var openMenu = document.getElementById('nav-toggler');
+    // openMenu.addEventListener("click", toggle);
 
 })();
 
